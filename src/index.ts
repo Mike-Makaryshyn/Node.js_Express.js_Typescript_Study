9f82868273f40978408ae596d60613704207eaea
Michael Makaryshyn <mi5hqaaaaov@gmail.com>
2023-04-17 15:13:07 +0300

    Initial

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import express, { Request, Response } from "express";
+
+const app = express();
+const port = 3000;
+
+interface TestFuncOutput {
+  title: string;
+  num: number | string;
+}
+
+const testFunc = (): TestFuncOutput => {
+  return {
+    title: "Random number",
+    num: Math.round(Math.random() * 10),
+  };
+};
+
+console.log(testFunc());
+
+app.get("/", (req: Request, res: Response) => {
+  let hello: string =
+    "<div style='height: 100%; display:flex; flex-direction: column; align-items: center; justify-content: center;'><h1>Hello Guys!</h1> <hr> <h2 style='margin: 0;'>This is my first <span style='color: green;'>Node.js</span> <span style='color: #F8DF1B;'>Express.js</span> <span style='color: #2D79C7;'>Typescript</span> App</p></div>";
+  res.send(hello);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
